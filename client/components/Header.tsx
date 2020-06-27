7b4f229dcbc3250ebac7e014bcdde68c0d3da311
aimanafzal <ayman.afzal@msn.com>
2020-06-27 18:04:36 +0500

    client folder deleted

--- client/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-/** @jsx jsx */
-import { jsx } from "@emotion/core";
-import { Title } from "./Title";
-
-export const Header = () => <header>
-    {/* <Title text="MultiLife!"/> */}
-    <p> </p>
-    
-</header>;
--- client/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {enableMapSet} from "immer";
-
-import {App} from './components/App';
-
-enableMapSet()
-
-ReactDOM.render(<App/>, document.getElementById('app'));
